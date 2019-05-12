5fe1e7d33a663f549d825e18a98b9fa0f72c1cee
franklioxygen <frank.li.oxygen@gmail.com>
2019-05-12 13:15:11 -0400

    832

---
+++ FlippingAnImage.js
@@ -0,0 +1,18 @@
+/**
+ * 832. Flipping an Image
+ * @param {number[][]} A
+ * @return {number[][]}
+ */
+var flipAndInvertImage = function (A) {
+    var B = A.map(function (arr) {
+        return arr.slice();
+    });
+    for (i = 0; i < A[0].length; i++) {
+        for (j = 0; j < A.length; j++) {
+
+            B[i][j] = ((A[i][A.length - j - 1] + 1) % 2);
+
+        }
+    }
+    return B;
+};
