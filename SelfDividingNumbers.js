227042acde224a22851589dddc2f4533a19b0aac
franklioxygen <frank.li.oxygen@gmail.com>
2019-05-13 09:33:03 -0400

    728

---
+++ SelfDividingNumbers.js
@@ -0,0 +1,28 @@
+/**
+ * 728. Self Dividing Numbers
+ * @param {number} left
+ * @param {number} right
+ * @return {number[]}
+ */
+var selfDividingNumbers = function (left, right) {
+    let res = [];
+    let curr;
+    let digits = [];
+    for (i = 0; i <= right - left; i++) {
+        curr = left + i;
+        for (j = 1; j < right.toString().length; j++) {
+            while ((Math.floor(curr / 10)) || (curr % 10)) {
+                digits.push(curr % 10);
+                curr = Math.floor(curr / 10);
+            }
+        }
+        while (digits.length != 0) {
+            if (((left + i) % digits.pop()) != 0) {
+                digits = [];
+                break;
+            }
+            if (digits.length == 0) res.push(left + i);
+        }
+    }
+    return res;
+};
