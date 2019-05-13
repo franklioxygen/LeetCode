7fbe216f432b61214efba831d34ff1b55beb6b8e
franklioxygen <frank.li.oxygen@gmail.com>
2019-05-12 22:42:03 -0400

    942

---
+++ DIStringMatch.js
@@ -0,0 +1,21 @@
+/**
+ * 942. DI String Match
+ * @param {string} S
+ * @return {number[]}
+ */
+var diStringMatch = function (S) {
+    let pos = 1;
+    let neg = -1;
+    let output = [0];
+    for (i = 0; i < S.length; i++) {
+        if (S[i] == "I") {
+            output.push(pos);
+            pos++;
+        } else {
+            output.push(neg);
+            neg--;
+        }
+    }
+    for (j = 0; j < output.length; j++) output[j] += Math.abs(neg + 1);
+    return output;
+};
