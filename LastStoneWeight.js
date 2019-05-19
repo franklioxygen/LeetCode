070a95fd7bbd085e2e834d569a939110d14121ee
franklioxygen <frank.li.oxygen@gmail.com>
2019-05-19 19:41:04 -0400

    1046

---
+++ LastStoneWeight.js
@@ -0,0 +1,18 @@
+/**
+ * 1046. Last Stone Weight
+ * @param {number[]} stones
+ * @return {number}
+ */
+var lastStoneWeight = function (stones) {
+    let sorted = stones.sort(sortNumber).reverse();
+    for (i = 0; sorted.length != 1; i++) {
+        sorted[1] = sorted[0] - sorted[1];
+        sorted.shift();
+        sorted.sort(sortNumber).reverse();
+    }
+    return sorted;
+};
+
+function sortNumber(a, b) {
+    return a - b;
+}
