115cbd33e573f4aed6027a22894169345e434c6c
franklioxygen <frank.li.oxygen@gmail.com>
2019-05-19 12:22:40 -0400

    575

---
+++ DistributeCandies.js
@@ -0,0 +1,10 @@
+/**
+ * 575. Distribute Candies
+ * @param {number[]} candies
+ * @return {number}
+ */
+var distributeCandies = function (candies) {
+    let kinds = new Set();
+    for (i in candies) kinds.add(candies[i]);
+    return Math.min(candies.length / 2, kinds.size);
+};
