4d3eaae72c9238682d59d6ddf56f056c6034b1d5
franklioxygen <frank.li.oxygen@gmail.com>
2019-05-20 16:09:45 -0400

    693

---
+++ BinaryNumberWithAlternatingBits.js
@@ -0,0 +1,11 @@
+/**
+ * 693. Binary Number with Alternating Bits
+ * @param {number} n
+ * @return {boolean}
+ */
+var hasAlternatingBits = function (n) {
+    s = n.toString(2);
+    for (i = 0; i < s.length - 1; i++)
+        if (s[i] == s[i + 1]) return false;
+    return true;
+};
