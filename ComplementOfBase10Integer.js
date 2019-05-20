534d8929dcf67bac54457b2c3b1f0d1977531df1
franklioxygen <frank.li.oxygen@gmail.com>
2019-05-20 14:50:43 -0400

    1009

---
+++ ComplementOfBase10Integer.js
@@ -0,0 +1,11 @@
+/**
+ * 1009. Complement of Base 10 Integer
+ * @param {number} N
+ * @return {number}
+ */
+var bitwiseComplement = function (N) {
+    let s = N.toString(2);
+    for (i = 0; i < s.length; i++)
+        s[i] == 0 ? s = s.substr(0, i) + '1' + s.substr(i + 1) : s = s.substr(0, i) + '0' + s.substr(i + 1);
+    return parseInt(s, 2);
+};
