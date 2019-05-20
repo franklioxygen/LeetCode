a57b1b9e50fd0ac1de564aca6887a91df7c36d75
franklioxygen <frank.li.oxygen@gmail.com>
2019-05-19 20:07:00 -0400

    412

---
+++ FizzBuzz.js
@@ -0,0 +1,24 @@
+/**
+ * 412. Fizz Buzz
+ * @param {number} n
+ * @return {string[]}
+ */
+var fizzBuzz = function (n) {
+    let res = [];
+
+    for (i = 1; i <= n; i++) {
+        if (i % 15 == 0) {
+            res.push("FizzBuzz");
+            continue;
+        }
+        if (i % 3 == 0) {
+            res.push("Fizz");
+            continue;
+        }
+        if (i % 5 == 0) {
+            res.push("Buzz");
+            continue;
+        } else res.push(i.toString());
+    }
+    return res;
+};
