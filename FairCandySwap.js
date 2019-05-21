921f3ab6975bc4ec803a0a493d0158b844a70261
franklioxygen <frank.li.oxygen@gmail.com>
2019-05-21 14:43:32 -0400

    888

---
+++ FairCandySwap.js
@@ -0,0 +1,23 @@
+/**
+ * 888. Fair Candy Swap
+ * @param {number[]} A
+ * @param {number[]} B
+ * @return {number[]}
+ */
+var fairCandySwap = function (A, B) {
+    let gap = (sum(A) - sum(B)) / 2;
+    for (i = 0; i < A.length; i++) {
+        for (j = 0; j < B.length; j++) {
+            if (A[i] - B[j] == gap) return [A[i], B[j]];
+        }
+    }
+};
+
+function sum(arr) {
+    let i;
+    let res = 0;
+    for (i = 0; i < arr.length; i++) {
+        res = res + arr[i];
+    }
+    return res;
+}
