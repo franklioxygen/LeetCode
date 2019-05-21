a19199f5d25f897cf36cb8b28ba898ea8fe4e47a
franklioxygen <frank.li.oxygen@gmail.com>
2019-05-21 17:06:51 -0400

    917

---
+++ ReverseOnlyLetters.js
@@ -0,0 +1,22 @@
+/**
+ * 917. Reverse Only Letters
+ * @param {string} S
+ * @return {string}
+ */
+var reverseOnlyLetters = function (S) {
+    let i = 0,
+        j = S.length - 1;
+
+    while (i < j) {
+        while ((!isLetter(S[i])) && i < j) i++;
+        while ((!isLetter(S[j])) && i < j) j--;
+        if (i != j) S = S.slice(0, i) + S[j] + S.slice(i + 1, j) + S[i] + S.slice(j + 1, S.length);
+        i++;
+        j--;
+    }
+    return S;
+};
+
+function isLetter(c) {
+    return c.toLowerCase() != c.toUpperCase();
+}
