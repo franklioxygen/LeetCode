a1f906ced60cc5405fcecf207995727f73c35932
franklioxygen <frank.li.oxygen@gmail.com>
2019-05-21 00:02:51 -0400

    976

---
+++ LargestPerimeterTriangle.js
@@ -0,0 +1,15 @@
+/**
+ * 976. Largest Perimeter Triangle
+ * @param {number[]} A
+ * @return {number}
+ */
+var largestPerimeter = function (A) {
+    A.sort(sortNumber).reverse();
+    for (i = 0; i < A.length; i++)
+        if (A[i] < A[i + 1] + A[i + 2]) return A[i] + A[i + 1] + A[i + 2];
+    return 0;
+};
+
+function sortNumber(a, b) {
+    return a - b;
+}
