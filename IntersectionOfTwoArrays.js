1e7b9caa5cc7ba659db3117f8671d70ebf77e083
franklioxygen <frank.li.oxygen@gmail.com>
2019-05-22 14:25:36 -0400

    349

---
+++ IntersectionOfTwoArrays.js
@@ -0,0 +1,20 @@
+/**
+ * 349. Intersection of Two Arrays
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersection = function (nums1, nums2) {
+    let res = new Set()
+    for (i = 0; i < nums1.length; i++) {
+        if (!res.has(nums1[i])) {
+            for (j = 0; j < nums2.length; j++) {
+                if (nums1[i] == nums2[j]) {
+                    res.add(nums1[i]);
+                    break;
+                }
+            }
+        }
+    }
+    return Array.from(res);
+};
