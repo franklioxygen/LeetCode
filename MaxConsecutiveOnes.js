37105266738f203423b7d05b4e9795bf8f19339f
franklioxygen <frank.li.oxygen@gmail.com>
2019-05-22 14:11:37 -0400

    485

---
+++ MaxConsecutiveOnes.js
@@ -0,0 +1,17 @@
+/**
+ * 485. Max Consecutive Ones
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMaxConsecutiveOnes = function (nums) {
+    let i;
+    let res = 0;
+    let curr = 0;
+    for (i = 0; i < nums.length; i++) {
+        if (nums[i] == 1) {
+            curr++;
+            res = res > curr ? res : curr;
+        } else curr = 0;
+    }
+    return res;
+};
