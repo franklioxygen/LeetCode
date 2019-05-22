f33201b6ef1d387718cc8f9f696991ac81d85ac2
franklioxygen <frank.li.oxygen@gmail.com>
2019-05-22 14:04:34 -0400

    896

---
+++ MonitonicArray.js
@@ -0,0 +1,20 @@
+/**
+ * 896. Monotonic Array
+ * @param {number[]} A
+ * @return {boolean}
+ */
+var isMonotonic = function (A) {
+    let B = A.slice(0);
+    return arraysEqual(B, A.sort(sortNum)) || arraysEqual(B, A.reverse());
+};
+
+function arraysEqual(a, b) {
+    for (let i = 0; i < a.length; ++i) {
+        if (a[i] !== b[i]) return false;
+    }
+    return true;
+}
+
+function sortNum(a, b) {
+    return a - b;
+}
