d2c59e059b66de7734ad42f35b54d6eb3fe58db6
franklioxygen <frank.li.oxygen@gmail.com>
2019-05-22 15:21:37 -0400

    283

---
+++ MoveZeros.js
@@ -0,0 +1,16 @@
+/**
+ * 283. Move Zeroes
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function (nums) {
+    let length = nums.length;
+
+    for (i = 0; i < nums.length && length != 0; length--) {
+        if (nums[i] == 0) {
+            nums.splice(i, 1);
+            nums.push(0);
+        } else i++;
+    }
+
+};
