665cdcd7f9c0576b0de5a181805e34b87b48802b
franklioxygen <frank.li.oxygen@gmail.com>
2019-05-24 23:24:30 -0400

    520

---
+++ DetectCapital.js
@@ -0,0 +1,19 @@
+/**
+ * 520. Detect Capital
+ * @param {string} word
+ * @return {boolean}
+ */
+var detectCapitalUse = function (word) {
+    if (word.length == 1) return true;
+    if (word[0] == word[0].toLowerCase() && word[1] == word[1].toUpperCase()) return false;
+    if (word[0] == word[0].toUpperCase() && word[1] == word[1].toUpperCase()) {
+        for (i = 2; i < word.length; i++)
+            if (word[i] != word[i].toUpperCase()) return false;
+    }
+    if ((word[0] == word[0].toLowerCase() && word[1] == word[1].toLowerCase()) ||
+        (word[0] == word[0].toUpperCase() && word[1] == word[1].toLowerCase())) {
+        for (i = 2; i < word.length; i++)
+            if (word[i] != word[i].toLowerCase()) return false;
+    }
+    return true;
+};
