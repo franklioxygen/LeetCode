b375c27c338858d55a01822dbec7975f6d9caf90
franklioxygen <frank.li.oxygen@gmail.com>
2019-05-24 23:11:53 -0400

    169

---
+++ MajorityElement.js
@@ -0,0 +1,12 @@
+/**
+ * 169. Majority Element
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function (nums) {
+    nums = nums.sort(sortNum);
+    return nums[Math.floor(nums.length / 2)];
+};
+let sortNum = (a, b) => {
+    return a - b;
+};
