ccb9e615d23307e17005d91cb95d2a76b3495be5
franklioxygen <frank.li.oxygen@gmail.com>
2019-05-26 12:18:55 -0400

    371

---
+++ SumOfTwoIntegers.js
@@ -0,0 +1,14 @@
+/**
+ * 371. Sum of Two Integers
+ * @param {number} a
+ * @param {number} b
+ * @return {number}
+ */
+var getSum = function (a, b) {
+    var c;
+    while ((c = a & b) != 0) {
+        a = a ^ b;
+        b = c << 1;
+    }
+    return a ^ b;
+};
