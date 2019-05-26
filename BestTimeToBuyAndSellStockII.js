0d1a14a4c6038fe070771dfd6ea4b1b4a20f33cd
franklioxygen <frank.li.oxygen@gmail.com>
2019-05-25 22:06:59 -0400

    122

---
+++ BestTimeToBuyAndSellStockII.js
@@ -0,0 +1,12 @@
+/**
+ * 122. Best Time to Buy and Sell Stock II
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function (prices) {
+    let res = 0;
+    for (i = 0; i < prices.length - 1; i++) {
+        if (prices[i + 1] > prices[i]) res = prices[i + 1] - prices[i] + res;
+    }
+    return res;
+};
