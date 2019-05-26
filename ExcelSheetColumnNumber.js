9c08ef007e3ca0eeef5313c066572f81eaed1ea1
franklioxygen <frank.li.oxygen@gmail.com>
2019-05-26 11:30:41 -0400

    171

---
+++ ExcelSheetColumnNumber.js
@@ -0,0 +1,13 @@
+/**
+ * 171. Excel Sheet Column Number
+ * @param {string} s
+ * @return {number}
+ */
+var titleToNumber = function (s) {
+    let res = 0;
+    for (j = 0, i = s.length - 1; i >= 0; i--, j++) res = res + letter2Number(s[i]) * Math.pow(26, j);
+    return res;
+};
+let letter2Number = char => {
+    return char.charCodeAt(0) - 64;
+};
