e9ec84ba4482fa7a95d2a713b1cfc4aa86f19d68
franklioxygen <frank.li.oxygen@gmail.com>
2019-05-25 21:51:16 -0400

    217

---
+++ ContainsDuplicate.js
@@ -0,0 +1,13 @@
+/**
+ * 217. Contains Duplicate
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function (nums) {
+    let numSet = new Set();
+    for (i = 0; i < nums.length; i++) {
+        if (!numSet.has(nums[i])) numSet.add(nums[i])
+        else return true;
+    }
+    return false;
+};
