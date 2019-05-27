48b44564875b6b70fa3114029c5bb42db0d685e9
franklioxygen <frank.li.oxygen@gmail.com>
2019-05-26 23:06:27 -0400

    1005

---
+++ MaximizeSumOfArrayAfterKNegations.js
@@ -0,0 +1,20 @@
+/**
+ * 1005. Maximize Sum Of Array After K Negations
+ * @param {number[]} A
+ * @param {number} K
+ * @return {number}
+ */
+var largestSumAfterKNegations = function (A, K) {
+    A = A.sort(sortNum);
+    for (i = 0; i < K; i++) {
+        A[0] = -A[0];
+        A = A.sort(sortNum);
+    }
+    return A.reduce(function (acc, curr) {
+        return acc + curr;
+    });
+};
+
+function sortNum(a, b) {
+    return a - b
+}
