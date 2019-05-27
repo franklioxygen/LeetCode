e3ac69125ece2b5eec906c9599f112bcc701cf21
franklioxygen <frank.li.oxygen@gmail.com>
2019-05-27 00:02:29 -0400

    717

---
+++ 1-bitAnd2-bitCharacters.js
@@ -0,0 +1,10 @@
+/**
+ * 717. 1-bit and 2-bit Characters
+ * @param {number[]} bits
+ * @return {boolean}
+ */
+var isOneBitCharacter = function (bits) {
+    let ones = 0;
+    for (i = bits.length - 2; i >= 0 && bits[i] == 1; i--) ones++;
+    return ones % 2 ? false : true;
+};
