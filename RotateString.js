96be381ca41b5a61cfb74e763cf30da76678764c
franklioxygen <frank.li.oxygen@gmail.com>
2019-05-28 13:05:19 -0400

    796

---
+++ RotateString.js
@@ -0,0 +1,13 @@
+/**
+ * 796. Rotate String
+ * @param {string} A
+ * @param {string} B
+ * @return {boolean}
+ */
+var rotateString = function (A, B) {
+    if (A == B) return true;
+    for (i = 1; i < A.length; i++) {
+        if (A.slice(i, A.length) + A.slice(0, i) == B) return true;
+    }
+    return false;
+};
