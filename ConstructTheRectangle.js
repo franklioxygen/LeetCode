9c2ddb7693c48bf8116e58fe72f59347498cdbc0
franklioxygen <frank.li.oxygen@gmail.com>
2019-05-28 14:52:33 -0400

    492

---
+++ ConstructTheRectangle.js
@@ -0,0 +1,12 @@
+/**
+ * 492. Construct the Rectangle
+ * @param {number} area
+ * @return {number[]}
+ */
+var constructRectangle = function (area) {
+    let w = Math.floor(Math.sqrt(area));
+    while (w > 0) {
+        if (area % w == 0) return [area / w, w];
+        w--;
+    }
+};
