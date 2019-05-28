6701a79508878f3002e10475891f7651db4cd3d7
franklioxygen <frank.li.oxygen@gmail.com>
2019-05-28 15:14:54 -0400

    455

---
+++ AssignCookies.js
@@ -0,0 +1,22 @@
+/**
+ * 455. Assign Cookies
+ * @param {number[]} g
+ * @param {number[]} s
+ * @return {number}
+ */
+var findContentChildren = function (g, s) {
+    let res = 0;
+    g.sort(sortNum);
+    s.sort(sortNum);
+    let child = 0;
+    for (i = 0; i < s.length; i++) {
+        if (s[i] >= g[child]) {
+            res++;
+            child++;
+        }
+    }
+    return res;
+};
+let sortNum = (a, b) => {
+    return a - b
+};
