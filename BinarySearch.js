5bca405258d0cbc0deb922b10b981b1969b755b3
franklioxygen <frank.li.oxygen@gmail.com>
2019-05-29 14:26:01 -0400

    704

---
+++ BinarySearch.js
@@ -0,0 +1,18 @@
+/**
+ * 704. Binary Search
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function (nums, target) {
+    let res;
+    let start = 0;
+    let end = nums.length - 1;
+    while (start <= end) {
+        res = Math.ceil((end + start) / 2);
+        if (nums[res] == target) return res;
+        if (nums[res] < target) start = res + 1;
+        else end = res - 1;
+    }
+    return -1;
+};
