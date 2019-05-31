a79f8c09ef9ee63aff352832a93fab099dbe5fb3
franklioxygen <frank.li.oxygen@gmail.com>
2019-05-31 08:00:11 -0400

    541

---
+++ ReverseStringII.js
@@ -0,0 +1,16 @@
+/**
+ * 541. Reverse String II
+ * @param {string} s
+ * @param {number} k
+ * @return {string}
+ */
+var reverseStr = function (s, k) {
+    let res = [];
+    let odd = 0;
+    for (i = 0; i < s.length; i += k) {
+        odd = odd == 1 ? 0 : 1;
+        res.push(s.slice(i, i + k));
+        if (odd == 1) res[i / k] = res[i / k].split("").reverse().join("");
+    }
+    return res.join("");
+};
