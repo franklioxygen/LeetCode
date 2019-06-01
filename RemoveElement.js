88349c993abdb362eec78aa67e17028f1cf18ae9
franklioxygen <frank.li.oxygen@gmail.com>
2019-06-01 19:09:26 -0400

    27

---
+++ RemoveElement.js
@@ -0,0 +1,13 @@
+/**
+ * 27. Remove Element
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function (nums, val) {
+    for (i = 0; i < nums.length;) {
+        if (nums[i] == val) nums.splice(i, 1);
+        else i++;
+    }
+    return nums.length
+};
