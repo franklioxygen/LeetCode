b2be871a9e36b7e696dbde7e1a335505e636df0b
franklioxygen <frank.li.oxygen@gmail.com>
2019-06-01 19:07:02 -0400

    551

---
+++ StudentAttendanceRecordI.js
@@ -0,0 +1,19 @@
+/**
+ * 551. Student Attendance Record I
+ * @param {string} s
+ * @return {boolean}
+ */
+var checkRecord = function (s) {
+    let abs = 0;
+
+    for (i = 0; i < s.length; i++) {
+        if (s[i] == "A") {
+            abs++;
+            if (abs > 1) return false;
+        }
+        if (s[i] == "L") {
+            if (s[i + 1] == "L" && s[i + 2] == "L") return false
+        }
+    }
+    return true;
+};
