bf6bc3a45eae85ecaa829f1bba49d5967591d126
franklioxygen <frank.li.oxygen@gmail.com>
2019-06-03 16:39:27 -0400

    101

---
+++ SymmetricTree.js
@@ -0,0 +1,23 @@
+/**
+ * 101. Symmetric Tree
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isSymmetric = function (root) {
+    if (root == null) return true;
+    return helper(root.left, root.right);
+};
+
+function helper(left, right) {
+    if (left == null && right == null) return true;
+    if (!left || !right) return false;
+    if (left.val != right.val) return false;
+    return helper(left.left, right.right) && helper(left.right, right.left);
+}
