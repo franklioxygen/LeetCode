8f19eb12442f0d84e83ebde634a228b34c5ac087
franklioxygen <frank.li.oxygen@gmail.com>
2019-06-02 20:51:46 -0400

    594

---
+++ LongestHarmoniousSubsequence.js
@@ -0,0 +1,22 @@
+/**
+ * 594. Longest Harmonious Subsequence
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findLHS = function (nums) {
+    let allNum = new Map();
+    let res = 0;
+    for (i = 0; i < nums.length; i++) {
+        if (!allNum.has(nums[i])) allNum.set(nums[i], 1);
+        else allNum.set(nums[i], allNum.get(nums[i]) + 1);
+    }
+    let arr = Array.from(allNum).sort((a, b) => {
+        return a[0] - b[0];
+    });
+    for (i = 0; i < arr.length - 1; i++) {
+        if (arr[i + 1][0] - arr[i][0] == 1)
+            res = res > (arr[i][1] + arr[i + 1][1]) ? res : (arr[i][1] + arr[i + 1][1]);
+    }
+    return res;
+
+};
