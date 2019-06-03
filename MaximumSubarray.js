3c66e4476788f4377ae0cd9b1816d0fbc516d112
franklioxygen <frank.li.oxygen@gmail.com>
2019-06-03 15:41:31 -0400

    53

---
+++ MaximumSubarray.js
@@ -0,0 +1,14 @@
+/**
+ * 53. Maximum Subarray
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function (nums) {
+    let curVal = 0;
+    let maxVal = nums[0];
+    for (i = 0; i < nums.length; i++) {
+        curVal = curVal > 0 ? curVal + nums[i] : nums[i];
+        maxVal = maxVal > curVal ? maxVal : curVal;
+    }
+    return maxVal;
+};
