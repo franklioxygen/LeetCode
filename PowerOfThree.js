637053fe1d45572236039805622ed203b4a5f375
franklioxygen <frank.li.oxygen@gmail.com>
2019-06-04 16:42:34 -0400

    326

---
+++ PowerOfThree.js
@@ -0,0 +1,12 @@
+/**
+ * 326. Power of Three
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfThree = function (n) {
+    while (n >= 1) {
+        if (n == 1) return true;
+        n = n / 3;
+    }
+    return false;
+};
