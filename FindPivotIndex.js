8f1de2ffad21a17ce87377aa72ad3bb737c8a816
franklioxygen <frank.li.oxygen@gmail.com>
2019-06-04 19:45:36 -0400

    724

---
+++ FindPivotIndex.js
@@ -0,0 +1,12 @@
+/**
+ * 724. Find Pivot Index
+ * @param {number[]} nums
+ * @return {number}
+ */
+var pivotIndex = function (nums) {
+    for (i = 0; i < nums.length; i++) {
+        if (nums.slice(0, i).reduce((sum, a) => sum += a, 0) ==
+            nums.slice(i + 1, nums.length).reduce((sum, a) => sum += a, 0)) return i;
+    }
+    return -1;
+};
