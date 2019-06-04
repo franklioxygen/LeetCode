f2743476be9daef501fa870efd2efecb092a1b75
franklioxygen <frank.li.oxygen@gmail.com>
2019-06-04 16:02:53 -0400

    819

---
+++ MostCommonWord.js
@@ -0,0 +1,19 @@
+/**
+ * 819. Most Common Word
+ * @param {string} paragraph
+ * @param {string[]} banned
+ * @return {string}
+ */
+var mostCommonWord = function (paragraph, banned) {
+    let m = new Map();
+    paragraph = paragraph.toLowerCase().replace(/\W/g, " ").replace(/  /g, " ").split(" ");
+    for (i = 0; i < paragraph.length; i++) {
+        if (!m.has(paragraph[i])) m.set(paragraph[i], 1);
+        else m.set(paragraph[i], m.get(paragraph[i]) + 1);
+    }
+    for (i = 0; i < banned.length; i++) m.delete(banned[i]);
+    m.delete("");
+    return Array.from(m).sort((a, b) => {
+        return a[1] - b[1]
+    }).reverse()[0][0];
+};
