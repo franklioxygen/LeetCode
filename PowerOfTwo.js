abc66d4de74945262b75b8065772e481c1c0b9a8
franklioxygen <frank.li.oxygen@gmail.com>
2019-06-04 16:06:44 -0400

    231

---
+++ PowerOfTwo.js
@@ -0,0 +1,12 @@
+/**
+ * 231. Power of Two
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfTwo = function (n) {
+    while (n >= 1) {
+        if (n == 1) return true;
+        n = n / 2;
+    }
+    return false;
+};
