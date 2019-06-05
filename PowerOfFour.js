e2449f29dde8605279d62514d072d02abfd5cc46
franklioxygen <frank.li.oxygen@gmail.com>
2019-06-05 15:22:04 -0400

    342

---
+++ PowerOfFour.js
@@ -0,0 +1,12 @@
+/**
+ * 342. Power of Four
+ * @param {number} num
+ * @return {boolean}
+ */
+var isPowerOfFour = function (num) {
+    while (num != 1) {
+        num /= 4;
+        if (num < 1) return false;
+    }
+    return true;
+};
