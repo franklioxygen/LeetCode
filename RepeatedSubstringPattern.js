6a1cde6fa2761b15a6225bdd45e58c7f66ab0534
franklioxygen <frank.li.oxygen@gmail.com>
2019-06-05 15:31:56 -0400

    459

---
+++ RepeatedSubstringPattern.js
@@ -0,0 +1,10 @@
+/**
+ * 459. Repeated Substring Pattern
+ * @param {string} s
+ * @return {boolean}
+ */
+var repeatedSubstringPattern = function (s) {
+    for (i = 1; i < (s.length + 1) / 2; i++)
+        if (s == s.slice(i, s.length) + s.slice(0, i)) return true;
+    return false;
+};
