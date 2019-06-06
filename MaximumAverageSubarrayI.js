970e60542b1b83923bf8d64e0aaf7511b5334ba7
franklioxygen <frank.li.oxygen@gmail.com>
2019-06-06 08:23:19 -0400

    643

---
+++ MaximumAverageSubarrayI.js
@@ -0,0 +1,18 @@
+/**
+ * 643. Maximum Average Subarray I
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var findMaxAverage = function (nums, k) {
+    let sub = [];
+    let max = -Infinity;
+    for (i = 0; i <= nums.length - k; i++) {
+        for (j = i; j < i + k; j++) {
+            sub.push(nums[j]);
+        }
+        max = Math.max(sub.reduce((a, b) => a + b, 0), max);
+        sub = [];
+    }
+    return max / k;
+};
