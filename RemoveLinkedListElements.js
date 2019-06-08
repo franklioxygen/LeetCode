4fa932f1591aa950f1be8dfedf53c6fb0a5b6e2c
franklioxygen <frank.li.oxygen@gmail.com>
2019-06-07 23:01:09 -0400

    203

---
+++ RemoveLinkedListElements.js
@@ -0,0 +1,25 @@
+/**
+ * 203. Remove Linked List Elements
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} val
+ * @return {ListNode}
+ */
+var removeElements = function (head, val) {
+
+    let dummy = {};
+    dummy.next = head;
+    let cur = dummy;
+    while (cur.next != null) {
+        if (cur.next.val != val) cur = cur.next;
+        else cur.next = cur.next.next
+    }
+    return dummy.next;
+
+};
