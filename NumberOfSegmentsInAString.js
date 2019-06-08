3ba0196a2cd13f8ebfb483c358f5079ae174e82f
franklioxygen <frank.li.oxygen@gmail.com>
2019-06-07 22:17:53 -0400

    434

---
+++ NumberOfSegmentsInAString.js
@@ -0,0 +1,10 @@
+/**
+ * 434. Number of Segments in a String
+ * @param {string} s
+ * @return {number}
+ */
+var countSegments = function (s) {
+    s = s.trim()
+    if (s.length === 0) return 0;
+    return s.replace(/\s+/g, ' ').split(' ').length;
+};
