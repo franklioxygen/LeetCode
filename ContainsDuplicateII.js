2432f92257cf4ae0820d9362fcc444938f50e578
franklioxygen <frank.li.oxygen@gmail.com>
2019-06-08 17:26:52 -0400

    219

---
+++ ContainsDuplicateII.js
@@ -0,0 +1,15 @@
+/**
+ * 219. Contains Duplicate II
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var containsNearbyDuplicate = function (nums, k) {
+    for (i = 0; i < nums.length; i++) {
+        for (j = i + 1; j < nums.length; j++) {
+            if (nums[i] == nums[j])
+                if (j - i <= k) return true;
+        }
+    }
+    return false;
+};
