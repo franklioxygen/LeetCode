55904d76b34a81d53e6d698d82f93937a4bdeb61
franklioxygen <frank.li.oxygen@gmail.com>
2019-06-08 18:57:52 -0400

    507

---
+++ PerfectNumber.js
@@ -0,0 +1,14 @@
+/**
+ * 507. Perfect Number
+ * @param {number} num
+ * @return {boolean}
+ */
+var checkPerfectNumber = function (num) {
+    if (num <= 1) return false;
+    let div = [];
+    for (i = num - 1; i > 0; i--)
+        if (num % i == 0) div.push(i);
+    return num == div.reduce(function (a, c) {
+        return a + c;
+    });
+};
