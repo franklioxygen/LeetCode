b84e742cb47bcef704c25e8ca4008591ee7139e4
franklioxygen <frank.li.oxygen@gmail.com>
2019-06-08 18:05:47 -0400

    941

---
+++ ValidMountainArray.js
@@ -0,0 +1,16 @@
+/**
+ * 941. Valid Mountain Array
+ * @param {number[]} A
+ * @return {boolean}
+ */
+var validMountainArray = function (A) {
+    if (A.length <= 2) return false;
+    let max = Math.max(...A);
+    let pos = A.indexOf(max);
+    if (pos == 0 || pos == A.length - 1) return false;
+    for (i = pos; i >= 1; i--)
+        if (A[i] <= A[i - 1]) return false;
+    for (i = pos; i <= A.length - 2; i++)
+        if (A[i] <= A[i + 1]) return false;
+    return true;
+};
