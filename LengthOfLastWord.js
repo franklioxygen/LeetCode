0e8e2fc655cbd8e0c5ed2fe3e210f4ee55e3163e
franklioxygen <frank.li.oxygen@gmail.com>
2019-06-09 22:46:30 -0400

    58

---
+++ LengthOfLastWord.js
@@ -0,0 +1,9 @@
+/**
+ * 58. Length of Last Word
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function (s) {
+    s = s.trim().split(" ");
+    return s[s.length - 1].length;
+};
