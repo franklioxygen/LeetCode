c934615c3c318b9d9683e6c4a1eddfe41d6f2d35
franklioxygen <frank.li.oxygen@gmail.com>
2019-06-11 08:18:27 -0400

    605

---
+++ CanPlaceFlowers.js
@@ -0,0 +1,20 @@
+/**
+ * 605. Can Place Flowers
+ * @param {number[]} flowerbed
+ * @param {number} n
+ * @return {boolean}
+ */
+var canPlaceFlowers = function (flowerbed, n) {
+    let res = 0;
+    for (i = 0; i < flowerbed.length; i++)
+        if (flowerbed[i] == 1) {
+            if (i != 0) flowerbed[i - 1] = 1;
+            if (i != flowerbed.length - 1) {
+                flowerbed[i + 1] = 1;
+                i++;
+            }
+        }
+    flowerbed = flowerbed.join("").replace(/1+/g, ",").split(",").filter(Boolean);
+    for (i = 0; i < flowerbed.length; i++) res = res + Math.ceil(flowerbed[i].length / 2);
+    return res >= n;
+};
