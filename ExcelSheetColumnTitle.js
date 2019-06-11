22ee7ce106415764ea4e4a4363479145eb7a5043
franklioxygen <frank.li.oxygen@gmail.com>
2019-06-11 16:07:29 -0400

    168

---
+++ ExcelSheetColumnTitle.js
@@ -0,0 +1,16 @@
+/**
+ * 168. Excel Sheet Column Title
+ * @param {number} n
+ * @return {string}
+ */
+var convertToTitle = function (n) {
+    let table = "ZABCDEFGHIJKLMNOPQRSTUVWXY";
+    let mod;
+    let res = "";
+    while (n > 0) {
+        mod = n % 26;
+        res = table[mod] + res;
+        n = Math.floor((n - 1) / 26);
+    }
+    return res;
+};
