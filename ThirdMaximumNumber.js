7c4beff8d5bd63fdd899e2329460b04ef40190ea
franklioxygen <frank.li.oxygen@gmail.com>
2019-06-11 16:16:59 -0400

    414

---
+++ ThirdMaximumNumber.js
@@ -0,0 +1,16 @@
+/**
+ * 414. Third Maximum Number
+ * @param {number[]} nums
+ * @return {number}
+ */
+var thirdMax = function (nums) {
+    nums.sort((a, b) => {
+        return a - b
+    })
+    let third = 3;
+    for (i = nums.length - 1; i >= 0; i--) {
+        if (nums[i] != nums[i - 1]) third--;
+        if (third == 0) return nums[i];
+    }
+    return nums[nums.length - 1];
+};
