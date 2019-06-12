aa4bbe000bcedb9f81cb94f5d157b72f5e1fc99b
franklioxygen <frank.li.oxygen@gmail.com>
2019-06-12 15:12:56 -0400

    921

---
+++ MinimumAddToMakeParenthesesValid.js
@@ -0,0 +1,20 @@
+/**
+ * 921. Minimum Add to Make Parentheses Valid
+ * @param {string} S
+ * @return {number}
+ */
+var minAddToMakeValid = function (S) {
+    let stack = [];
+    for (pa of S) {
+        if (pa == "(") {
+            stack.push("(");
+            continue;
+        } else {
+            if (stack[stack.length - 1] == "(") {
+                stack.pop();
+                continue;
+            } else stack.push(")");
+        }
+    }
+    return stack.length;
+};
