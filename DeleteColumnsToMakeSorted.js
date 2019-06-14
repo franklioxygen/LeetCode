1793897a167667e1bb73fb0858175da8855831da
franklioxygen <frank.li.oxygen@gmail.com>
2019-06-14 11:57:13 -0400

    944

---
+++ DeleteColumnsToMakeSorted.js
@@ -0,0 +1,17 @@
+/**
+ * 944. Delete Columns to Make Sorted
+ * @param {string[]} A
+ * @return {number}
+ */
+var minDeletionSize = function (A) {
+    let res = 0;
+    for (i = 0; i < A[0].length; i++) {
+        for (j = 0; j < A.length - 1; j++) {
+            if (A[j][i].charCodeAt(0) > A[j + 1][i].charCodeAt(0)) {
+                res++;
+                break;
+            }
+        }
+    }
+    return res;
+};
