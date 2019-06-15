5252d73ff9a211ade0039a4bf05d49210e4a1a02
franklioxygen <frank.li.oxygen@gmail.com>
2019-06-15 01:35:07 -0400

    912

---
+++ SortAnArray.js
@@ -0,0 +1,21 @@
+/**
+ * 912. Sort an Array
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortArray = function (nums) {
+    let res = [];
+    while (nums.length != 0) {
+        let min = Infinity;
+        let index = 0;
+        for (i = 0; i < nums.length; i++) {
+            if (min > nums[i]) {
+                min = nums[i];
+                index = i;
+            }
+        }
+        nums.splice(index, 1);
+        res.push(min);
+    }
+    return res;
+};
