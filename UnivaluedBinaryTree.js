1879540a56865127b6a3a930908d30889611ff5b
franklioxygen <frank.li.oxygen@gmail.com>
2019-06-16 10:00:35 -0400

    965

---
+++ UnivaluedBinaryTree.js
@@ -0,0 +1,21 @@
+/**
+ * 965. Univalued Binary Tree
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isUnivalTree = function (root) {
+    if (!root) return true;
+    let rootVal = root.val;
+    let check = node => {
+        if (!node) return true;
+        return (node.val == rootVal) && check(node.left) && check(node.right);
+    }
+    return check(root);
+};
