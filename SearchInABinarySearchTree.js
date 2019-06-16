64bf81a90cee4c4c1a79f21b52fe4ed8083d38df
franklioxygen <frank.li.oxygen@gmail.com>
2019-06-16 09:59:54 -0400

    700

---
+++ SearchInABinarySearchTree.js
@@ -0,0 +1,20 @@
+/**
+ * 700. Search in a Binary Search Tree
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} val
+ * @return {TreeNode}
+ */
+var searchBST = function (root, val) {
+    if (!root) return null;
+    if (root.val == val) return root;
+    if (root.val < val) return searchBST(root.right, val);
+    else return searchBST(root.left, val);
+
+};
