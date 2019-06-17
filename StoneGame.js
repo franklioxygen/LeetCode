6a10f05b19d22f49b51f892378f5a81840b0a268
franklioxygen <frank.li.oxygen@gmail.com>
2019-06-17 13:17:18 -0400

    877

---
+++ StoneGame.js
@@ -0,0 +1,17 @@
+/**
+ * 877. Stone Game
+ * @param {number[]} piles
+ * @return {boolean}
+ */
+var stoneGame = function (piles) {
+    piles.sort((a, b) => {
+        return a - b
+    });
+    let odd = 0;
+    let even = 0;
+    for (i = 0; i < piles.length; i++) {
+        if (i % 2 != 0) odd += piles[i];
+        else even += piles[i];
+    }
+    return odd > even;
+};
