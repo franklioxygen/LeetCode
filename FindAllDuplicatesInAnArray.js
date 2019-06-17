d1dd0d97fc714c108cead04ca82b8c42d308e981
franklioxygen <frank.li.oxygen@gmail.com>
2019-06-17 15:48:27 -0400

    442

---
+++ FindAllDuplicatesInAnArray.js
@@ -0,0 +1,14 @@
+/**
+ * 442. Find All Duplicates in an Array
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var findDuplicates = function (nums) {
+    let dup = new Set();
+    let res = [];
+    for (num of nums) {
+        if (!dup.has(num)) dup.add(num);
+        else res.push(num);
+    }
+    return res;
+};
