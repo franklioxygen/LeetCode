e4fbf63c1276e29b7de9e4847d49f0069b220c2d
franklioxygen <frank.li.oxygen@gmail.com>
2019-06-18 08:39:22 -0400

    739

---
+++ DailyTemperatures.js
@@ -0,0 +1,19 @@
+/**
+ * 739. Daily Temperatures
+ * @param {number[]} T
+ * @return {number[]}
+ */
+var dailyTemperatures = function (T) {
+    let res = [];
+    T.push(0);
+    for (i = 0; i < T.length; i++) {
+        for (j = i + 1; j < T.length; j++) {
+            if (T[j] > T[i]) {
+                res.push(j - i);
+                break;
+            }
+            if (j == T.length - 1) res.push(0);
+        }
+    }
+    return res;
+};
