c4b0e7abcc46974c099c47629f5ef78d8235f786
franklioxygen <frank.li.oxygen@gmail.com>
2019-06-19 21:24:36 -0400

    876

---
+++ MiddleOfTheLinkedList.js
@@ -0,0 +1,21 @@
+/**
+ * 876. Middle of the Linked List
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var middleNode = function (head) {
+    let slow = head;
+    let fast = head;
+    while (fast && fast.next) {
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+    return slow;
+};
