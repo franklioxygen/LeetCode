48be3580a43040529ab2a23d6679af9d2dcb79eb
franklioxygen <frank.li.oxygen@gmail.com>
2019-06-20 23:25:54 -0400

    789

---
+++ EscapeTheGhosts.js
@@ -0,0 +1,11 @@
+/**
+ * 789. Escape The Ghosts
+ * @param {number[][]} ghosts
+ * @param {number[]} target
+ * @return {boolean}
+ */
+var escapeGhosts = function (ghosts, target) {
+    for (gho of ghosts)
+        if (Math.abs(gho[0] - target[0]) + Math.abs(gho[1] - target[1]) <= Math.abs(target[0]) + Math.abs(target[1])) return false;
+    return true;
+};
