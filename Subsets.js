e2ad7dd86b9ea45ea886dc108ac1cd8e5b9d3910
franklioxygen <frank.li.oxygen@gmail.com>
2019-06-24 16:19:52 -0400

    78

---
+++ Subsets.js
@@ -0,0 +1,22 @@
+/**
+ * 78. Subsets
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var subsets = function (nums) {
+    let res = [];
+    let dfs = (n, start, cur) => {
+        if (cur.length == n) {
+            let dummy = JSON.parse(JSON.stringify(cur));
+            res.push(dummy);
+            return;
+        }
+        for (let i = start; i < nums.length; i++) {
+            cur.push(nums[i]);
+            dfs(n, i + 1, cur);
+            cur.pop();
+        }
+    }
+    for (i = 0; i <= nums.length; i++) dfs(i, 0, []);
+    return res;
+};
