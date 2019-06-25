4aaec2e8f8cd1fcbac414df41a95a6257cfe34d5
franklioxygen <frank.li.oxygen@gmail.com>
2019-06-25 11:51:47 -0400

    462

---
+++ MinimumMovesToEqualArrayElementsII.js
@@ -0,0 +1,16 @@
+/**
+ * 462. Minimum Moves to Equal Array Elements II
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minMoves2 = function (nums) {
+    let res = 0;
+    nums.sort((a, b) => {
+        return a - b;
+    })
+    let target = nums[Math.floor(nums.length / 2)];
+    for (num of nums) {
+        res += (Math.abs(num - target))
+    }
+    return res;
+};
