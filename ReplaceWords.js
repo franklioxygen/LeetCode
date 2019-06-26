7de38e2197d4d165c4844d6d5d861e83f284c238
franklioxygen <frank.li.oxygen@gmail.com>
2019-06-26 12:15:05 -0400

    648

---
+++ ReplaceWords.js
@@ -0,0 +1,15 @@
+/**
+ * 648. Replace Words
+ * @param {string[]} dict
+ * @param {string} sentence
+ * @return {string}
+ */
+var replaceWords = function (dict, sentence) {
+    sentence = sentence.split(" ");
+    for (root of dict) {
+        for (i = 0; i < sentence.length; i++) {
+            if (sentence[i].slice(0, root.length) == root) sentence[i] = root;
+        }
+    }
+    return sentence.join(" ");
+};
