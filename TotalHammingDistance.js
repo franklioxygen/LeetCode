cf78eb6a8ba06410afa08b46229c68b45dcd2db5
franklioxygen <frank.li.oxygen@gmail.com>
2019-06-28 19:24:21 -0400

    477

---
+++ TotalHammingDistance.js
@@ -0,0 +1,14 @@
+/**
+ * 477. Total Hamming Distance
+ * @param {number[]} nums
+ * @return {number}
+ */
+var totalHammingDistance = function (nums) {
+    var res = 0;
+    for (var i = 0; i < 32; i++) {
+        var one = 0;
+        for (num of nums) one += num >> i & 1;
+        res += one * (nums.length - one);
+    }
+    return res;
+};
