9e6587bbd21436d992bd44f156b6cc5210056091
franklioxygen <frank.li.oxygen@gmail.com>
2019-06-27 21:00:42 -0400

    677

---
+++ MapSumPairs.js
@@ -0,0 +1,36 @@
+/**
+ * 677. Map Sum Pairs
+ * Initialize your data structure here.
+ */
+var MapSum = function () {
+    this.sumMap = new Map();
+};
+
+/** 
+ * @param {string} key 
+ * @param {number} val
+ * @return {void}
+ */
+MapSum.prototype.insert = function (key, val) {
+    this.sumMap.set(key, val);
+    return;
+};
+
+/** 
+ * @param {string} prefix
+ * @return {number}
+ */
+MapSum.prototype.sum = function (prefix) {
+    let sum = 0;
+    let len = prefix.length;
+    for (el of this.sumMap)
+        if (el[0].slice(0, len) == prefix) sum += el[1];
+    return sum;
+};
+
+/** 
+ * Your MapSum object will be instantiated and called as such:
+ * var obj = new MapSum()
+ * obj.insert(key,val)
+ * var param_2 = obj.sum(prefix)
+ */
