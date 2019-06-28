6237e3eb242dcae5c5c3b2114c6acff9fe09a26c
franklioxygen <frank.li.oxygen@gmail.com>
2019-06-28 12:02:04 -0400

    328

---
+++ OddEvenLinkedList.js
@@ -0,0 +1,26 @@
+/**
+ * 328. Odd Even Linked List
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var oddEvenList = function (head) {
+    if (!head) return head;
+    let odd = head;
+    let even = head.next;
+    let evenHead = head.next;
+    while (odd.next && even.next) {
+        odd.next = odd.next.next;
+        even.next = even.next.next;
+        odd = odd.next;
+        even = even.next;
+    }
+    odd.next = evenHead;
+    return head;
+};
