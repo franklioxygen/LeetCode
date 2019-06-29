d9db2e97dd4107115dbda1c81d2fb5545830e334
franklioxygen <frank.li.oxygen@gmail.com>
2019-06-29 12:38:01 -0400

    714

---
+++ BestTimeToBuyAndSellStockWithTransactionFee.js
@@ -0,0 +1,15 @@
+/**
+ * 714. Best Time to Buy and Sell Stock with Transaction Fee
+ * @param {number[]} prices
+ * @param {number} fee
+ * @return {number}
+ */
+var maxProfit = function (prices, fee) {
+    let buy = -prices[0];
+    let sell = 0;
+    for (price of prices) {
+        buy = Math.max(buy, sell - price);
+        sell = Math.max(sell, buy + price - fee);
+    }
+    return sell;
+};
