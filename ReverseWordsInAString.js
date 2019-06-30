17ece0d3890271cb584f73fa4915c37700084348
franklioxygen <frank.li.oxygen@gmail.com>
2019-06-30 09:57:18 -0400

    151

---
+++ ReverseWordsInAString.js
@@ -0,0 +1,9 @@
+/**
+ * 151. Reverse Words in a String
+ * @param {string} s
+ * @return {string}
+ */
+var reverseWords = function (s) {
+    s = s.replace(/\s+/g, ' ').trim().split(" ").reverse().join(" ");
+    return s;
+};
