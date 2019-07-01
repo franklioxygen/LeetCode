afc12a7e7e999aa5a956a91484dae071ef4ef0e4
franklioxygen <frank.li.oxygen@gmail.com>
2019-07-01 16:21:13 -0400

    226

---
+++ InvertBinaryTree.js
@@ -0,0 +1,22 @@
+/**
+ * 226. Invert Binary Tree
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var invertTree = function (root) {
+    if (!root) return root;
+    let helper = node => {
+        [node.left, node.right] = [node.right, node.left];
+        if (node.left) helper(node.left);
+        if (node.right) helper(node.right);
+    };
+    helper(root);
+    return root;
+};
