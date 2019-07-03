c459fb7c3864f637663a4fe0c61752040742eb76
franklioxygen <frank.li.oxygen@gmail.com>
2019-07-03 10:36:03 -0400

    543

---
+++ DiameterOfBinaryTree.js
@@ -0,0 +1,25 @@
+/**
+ * 543. Diameter of Binary Tree
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var diameterOfBinaryTree = function (root) {
+    if (!root) return 0;
+    let res = 0;
+    let helper = node => {
+        if (!node) return 0;
+        let leftNode = helper(node.left);
+        let rightNode = helper(node.right);
+        res = Math.max(leftNode + rightNode + 1, res)
+        return Math.max(leftNode, rightNode) + 1;
+    };
+    helper(root);
+    return res - 1
+};
