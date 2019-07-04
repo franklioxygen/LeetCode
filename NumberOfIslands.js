24af3bc93ac95f4fc834e7449bb1fc72142b396d
franklioxygen <frank.li.oxygen@gmail.com>
2019-07-04 14:01:28 -0400

    200

---
+++ NumberOfIslands.js
@@ -0,0 +1,29 @@
+/**
+ * 200. Number of Islands
+ * @param {character[][]} grid
+ * @return {number}
+ */
+var numIslands = function (grid) {
+    let res = 0;
+    let bfs = (x, y) => {
+        grid[x][y] = 2;
+        dirs = [
+            [x + 1, y],
+            [x - 1, y],
+            [x, y + 1],
+            [x, y - 1]
+        ];
+        for ([a, b] of dirs)
+            if (a > -1 && b > -1 && a < grid.length && b < grid[0].length && grid[a][b] == 1)
+                bfs(a, b);
+    };
+    for (i = 0; i < grid.length; i++) {
+        for (j = 0; j < grid[0].length; j++) {
+            if (grid[i][j] == 1) {
+                bfs(i, j);
+                res++;
+            }
+        }
+    }
+    return res;
+};
