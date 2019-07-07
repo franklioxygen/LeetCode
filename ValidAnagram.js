641f306b62389112aa86fc2088978f67d9c90da1
franklioxygen <frank.li.oxygen@gmail.com>
2019-07-07 15:37:05 -0400

    242

--- ValidAnagram.js
+++ ValidAnagram.js
@@ -5,21 +5,5 @@
  * @return {boolean}
  */
 var isAnagram = function (s, t) {
-    if (s.length == 0 && t.length == 0) return true;
-    if (s.length != t.length) return false;
-    for (i = 0; i < s.length;) {
-        for (j = 0; j < t.length; j++) {
-            if (s[i] == t[j]) {
-                s = s.slice(0, i) + s.slice(i + 1, s.length);
-                t = t.slice(0, j) + t.slice(j + 1, t.length);
-                break;
-            }
-            if ((s[i] != t[j]) && (j == t.length - 1)) {
-                return false;
-            }
-
-        }
-
-    }
-    return true;
+    return [...s].sort().join("") === [...t].sort().join("");
 };
