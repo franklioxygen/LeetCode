38f9bbb204e4d7b2b0d93222436c4fd768b8caa4
franklioxygen <frank.li.oxygen@gmail.com>
2019-07-08 16:01:41 -0400

    701

---
+++ InsertIntoABinarySearchTree.js
@@ -0,0 +1,34 @@
+/**
+ * 701. Insert into a Binary Search Tree
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} val
+ * @return {TreeNode}
+ */
+var insertIntoBST = function (root, val) {
+    let helper = node => {
+        if (node.val > val) {
+            if (node.left) helper(node.left);
+            else {
+                node.left = new TreeNode();
+                node.left.val = val;
+                return;
+            }
+        } else {
+            if (node.right) helper(node.right);
+            else {
+                node.right = new TreeNode();
+                node.right.val = val;
+                return;
+            }
+        }
+    };
+    helper(root);
+    return root;
+};
