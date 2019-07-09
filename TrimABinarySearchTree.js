7be7543305636c818382912c5bf8a5728a8b5fde
franklioxygen <frank.li.oxygen@gmail.com>
2019-07-09 11:44:33 -0400

    669

---
+++ TrimABinarySearchTree.js
@@ -0,0 +1,25 @@
+/**
+ * 669. Trim a Binary Search Tree
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} L
+ * @param {number} R
+ * @return {TreeNode}
+ */
+var trimBST = function (root, L, R) {
+    let helper = node => {
+        if (node == null) return null;
+        if (node.val < L) return helper(node.right);
+        if (node.val > R) return helper(node.left);
+        node.left = helper(node.left);
+        node.right = helper(node.right);
+        return node;
+    };
+    return helper(root)
+};
