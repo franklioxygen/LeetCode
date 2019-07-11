7ec97ba0c0d9684d29e6da9647829212edbc3b9d
franklioxygen <frank.li.oxygen@gmail.com>
2019-07-11 09:46:00 -0400

    814

---
+++ BinaryTreePruning.js
@@ -0,0 +1,21 @@
+/**
+ * 814. Binary Tree Pruning
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var pruneTree = function (root) {
+    let helper = (node) => {
+        if (!node) return node;
+        node.left = helper(node.left);
+        node.right = helper(node.right);
+        return (node.val == 1 || node.left || node.right) ? node : null;
+    };
+    return helper(root);
+};
