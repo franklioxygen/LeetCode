5c801f8a83da37f49b6526fc1f5ca6d0aa84aebb
Peifan Li <frank.li.oxygen@gmail.com>
2019-07-13 22:55:49 -0700

    687

---
+++ LongestUnivaluePath.js
@@ -0,0 +1,28 @@
+/**
+ * 687. Longest Univalue Path
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var longestUnivaluePath = function (root) {
+    let res = 0;
+    let helper = node => {
+        if (!node) return 0;
+        let left = helper(node.left);
+        let right = helper(node.right);
+        let leftPath = 0;
+        let rightPath = 0;
+        if (node.left && node.val === node.left.val) leftPath = left + 1;
+        if (node.right && node.val == node.right.val) rightPath = right + 1;
+        res = Math.max(res, leftPath + rightPath);
+        return Math.max(leftPath, rightPath)
+    };
+    helper(root);
+    return res;
+};
