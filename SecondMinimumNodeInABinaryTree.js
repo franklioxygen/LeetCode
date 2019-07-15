0ded7f5afd4a85503b6d90669838b71740336cab
franklioxygen <frank.li.oxygen@gmail.com>
2019-07-15 09:45:23 -0400

    671

---
+++ SecondMinimumNodeInABinaryTree.js
@@ -0,0 +1,27 @@
+/**
+ * 671. Second Minimum Node In a Binary Tree
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var findSecondMinimumValue = function (root) {
+    let valSet = new Set();
+
+    let helper = node => {
+        if (!valSet.has(node.val)) valSet.add(node.val);
+        if (node.left) helper(node.left);
+        if (node.right) helper(node.right);
+    };
+    helper(root);
+    valSet = Array.from(valSet).sort((a, b) => {
+        return a - b
+    }).sort();
+    if (valSet.length > 1) return valSet[1];
+    else return -1;
+};
