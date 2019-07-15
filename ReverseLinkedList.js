ba3012a22a72d427cfd4bb1da8554840c29077fa
franklioxygen <frank.li.oxygen@gmail.com>
2019-07-15 09:44:54 -0400

    206

---
+++ ReverseLinkedList.js
@@ -0,0 +1,30 @@
+/**
+ * 206. Reverse Linked List
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function (head) {
+    if (!head) return head;
+    let temp = [];
+    while (head != null) {
+        temp.push(head.val);
+        head = head.next;
+    }
+    let newHead = new ListNode()
+    let currNode = newHead;
+    while (temp.length != 0) {
+        currNode.val = temp.pop();
+        if (temp.length != 0) {
+            currNode.next = new ListNode();
+            currNode = currNode.next;
+        }
+    }
+    return newHead;
+};
