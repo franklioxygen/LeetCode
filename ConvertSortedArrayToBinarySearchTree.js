c289b02ea8144abe01c7aaf6084fabc56396b51b
Peifan Li <frank.li.oxygen@gmail.com>
2022-01-26 15:12:37 -0500

    108

---
+++ ConvertSortedArrayToBinarySearchTree.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {number[]} nums
+ * @return {TreeNode}
+ */
+var sortedArrayToBST = function(nums) {
+    let treeNode = new TreeNode;
+    treeNode.val= nums[Math.floor(nums.length/2)]; 
+    let leftNums = nums.slice(0,Math.floor(nums.length/2));
+    let rightNums = nums.slice(Math.floor(nums.length/2)+1, nums.length);
+    if(leftNums.length<=0) {treeNode.left = null;}
+    else {
+        treeNode.left = sortedArrayToBST(leftNums);
+    }
+    if(rightNums.length<=0) {treeNode.right = null;}
+    else {
+        treeNode.right = sortedArrayToBST(rightNums);
+    }
+    return treeNode
+};
