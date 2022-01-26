ea2e3c2bff6bd47afdbbfbf000eac6f0b813a5ac
Peifan Li <frank.li.oxygen@gmail.com>
2022-01-26 15:35:06 -0500

    141

---
+++ LinkedListCycle.js
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function(head) {
+    let set = new Set();
+    while(head && head.next != null){
+        if(set.has(head.next)) return true;
+        else {
+            set.add(head);
+            head = head.next;         
+        }
+    }
+    return false;  
+};
