92e312eb6fac47ecc79e4a215495374d8b73cbe6
Peifan Li <frank.li.oxygen@gmail.com>
2022-01-26 15:13:45 -0500

    83

---
+++ RemoveDuplicatesFromSortedList.js
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteDuplicates = function(head) {
+    let pointer = head;
+    while(head && pointer.next != null) {
+        if(pointer.val == pointer.next.val) {
+            pointer.next = pointer.next.next;
+        } else {
+            pointer = pointer.next;
+        }
+    }
+    return head;
+};
