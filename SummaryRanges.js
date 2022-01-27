4258b3960985422ab6e98ebf5c199c14dab3f86d
Peifan Li <frank.li.oxygen@gmail.com>
2022-01-27 16:46:21 -0500

    228

---
+++ SummaryRanges.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {string[]}
+ */
+var summaryRanges = function(nums) {
+    let res = [];
+    for(i=0;i<nums.length;i++){
+        if(nums[i]+1!=nums[i+1]){
+            let end = nums[i]&&i!=0?"->"+nums[i]:"";
+            res.push(nums[0]+end);
+            nums = nums.slice(i+1);
+            i=-1;
+        }
+    }
+    return res;
+};
